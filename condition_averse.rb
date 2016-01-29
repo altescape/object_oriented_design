0157eb2bea1b784fe9bc89ccb3d4aba38ba26022
altescape <hello@michaelwatts.me>
2016-01-29 15:22:46 +0000

    add condition averse example

--- condition_averse.rb
+++ condition_averse.rb
@@ -1,11 +1,31 @@
+# https://www.youtube.com/watch?v=9lv2lBq6x4A
+#
+# Sandi Metz Nothing is something
 class Animal
-  def name
-  # . . .
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.find(id)
+    if id == ''
+      nil
+    else
+      new(id)
+    end
   end
 end
 
-class NoAnimal
+class MissingAnimal
+  attr_accessor :name
   def name
     "No animal"
   end
-end  
+end
+
+ids = ['pig', '', 'sheep']
+
+animals = ids.map {|id| Animal.find(id) || MissingAnimal.new}
+puts animals
+animals.each {|animal| puts animal.name}
