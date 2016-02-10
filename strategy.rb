b982780397317fbaf6a15fcadc851a8d8975ac95
altescape <hello@michaelwatts.me>
2016-02-10 11:59:02 +0000

    added strategy

---
+++ strategy.rb
@@ -0,0 +1,54 @@
+class Food
+  def type
+    raise NotImplementedError, 'Ask the subclass'
+  end
+end
+
+class HotDog < Food
+  def type
+    "hot dog"
+  end
+end
+
+class Hamburger < Food
+  def type
+    "hamburger"
+  end
+end
+
+class VeggieBurger < Food
+  def type
+    "veggieburger"
+  end
+end
+
+class Grill
+  attr_accessor :food
+  
+  def initialize food
+    @food = food
+  end
+
+  def grilling
+    "Grilling the #{food.type}"
+  end
+
+  private
+  
+  def print_food
+    food_is_string? ? food : food.type
+  end
+
+  def food_is_string?
+    food.is_a? String
+  end
+end
+
+grill = Grill.new(HotDog.new)
+grill.grilling
+
+grill.food = Hamburger.new
+grill.grilling
+
+grill.food = VeggieBurger.new
+grill.grilling
